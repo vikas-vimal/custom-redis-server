e385de876ccf18b89b87e748f4d8287184ab0889
Vikki <vikas@hood.live>
2024-04-13 23:44:33 +0530

    started returning OK on redis commands

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
   console.log("Client connected...");
+
+  socket.on("data", (dataBuffer) => {
+    console.log("Received:", dataBuffer.toString());
+    socket.write("+OK\r\n");
+  });
 });
 
 server.listen(8379, () => console.log("Custom Redis Server started on port 8379"));
